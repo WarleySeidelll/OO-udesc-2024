a3d8459e3a40a234dfa9c89be44e5dc4f350f573
Warley Seidel <warley.s@edu.udesc.br>
2024-08-19 21:48:43 -0300

    Classe OlaMundo e respectivo teste

---
+++ src/OlaMundo.java
@@ -0,0 +1,15 @@
+public class OlaMundo {
+    String nome = "Udesc!";
+    int  idade = 42;
+
+    void imprimirdados(){
+
+        System.out.println(nome + " - " + idade);
+    }
+void imprimir(String Nome, int Idade){
+
+        System.out.println(Nome + " - " + Idade);
+}
+
+
+}
---
+++ src/TestaOlaMundo.java
@@ -0,0 +1,9 @@
+public class TestaOlaMundo {
+
+    public static void main(String[] args) {
+
+        OlaMundo ola = new OlaMundo();
+        ola.imprimirdados();
+        ola.imprimir( "Fulano", 18);
+    }
+}
